ae0127626dc3d51972de432ccf7224bfbd0aa397
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-30 05:48:09 -0300

    feat: Projeto iniciado

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import Style from "./header.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <header className={Style.header}>
+       <img src="./logotipo.svg" alt="" />
+        <nav>
+          <ul>
+            <li>SOBRE</li>
+            <li>PREÇO</li>
+            <li>FAQ</li>
+          </ul>
+        </nav>
+
+        <button>
+            Entrar
+        </button>
+      </header>
+    </>
+  );
+}
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import { HandHeart } from "lucide-react";
+import Style from "./hero.module.css";
+export default function Hero() {
+  return (
+    <>
+      <main className={Style.main}>
+
+        <div className={Style.cto}>
+          <h1>Não dê apenas um presente</h1>
+          <h2>Eternize uma história</h2>
+        </div>
+
+        <section>
+         <img src="./iphone.svg" alt="" />
+        <section className={Style.conteudo}>
+       
+        </section>
+        </section>
+         
+        
+
+
+      </main>
+    </>
+  );
+}
